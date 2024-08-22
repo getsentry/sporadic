bfb8ce373759c286fbb5cee21ce95cd7d6bc1e63
Neel Shah <neelshah.sa@gmail.com>
2024-08-22 15:32:42 +0200

    Working blip feed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,80 @@
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
-// import "./user_socket.js"
+// import './user_socket.js'
 
 // You can include dependencies in two ways.
 //
 // The simplest option is to put them in assets/vendor and
 // import them using relative paths:
 //
-//     import "../vendor/some-package.js"
+//     import '../vendor/some-package.js'
 //
 // Alternatively, you can `npm install some-package --prefix assets` and import
 // them using a path starting with the package name:
 //
-//     import "some-package"
+//     import 'some-package'
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import 'phoenix_html'
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import {Socket} from 'phoenix'
+import {LiveSocket} from 'phoenix_live_view'
+import topbar from '../vendor/topbar'
 
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 const GEOJSON = 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_land.geojson'
+const MAPPING = { 'ruby': 'red', 'python': 'blue', 'javascript': 'yellow' }
+const BUFFER_MAX = 1000
 
 let Hooks = {
     DrawMap: {
         mounted() {
-            let projection = d3.geoNaturalEarth1();
+            let projection = d3.geoNaturalEarth1()
             let geoGenerator = d3.geoPath().projection(projection)
-            let svg = d3.select('#map_container').append('svg').attr('class', 'w-full h-full')
-            let g = svg.append('g').attr('id', 'map')
+            let svg = d3.select(this.el)
+            let map_group = svg.append('g').attr('id', 'map')
+            let blips_group = svg.append('g').attr('id', 'blips')
 
             d3.json(GEOJSON).then((json) => {
-                g.selectAll('path')
+                map_group.selectAll('path')
                     .data(json.features)
                     .join('path')
                     .attr('d', geoGenerator)
                     .attr('class', 'fill-violet-200 stroke-neutral-400')
             })
+
+            this.handleEvent('feed', entry => {
+                if (blips_group.selectChildren().size() > BUFFER_MAX) {
+                    blips_group.selectChild().remove()
+                }
+
+                let { latitude, longitude, time, platform } = entry
+                let [ long, lat ] = projection([longitude, latitude])
+
+                blips_group.append('circle')
+                    .attr('id', time)
+                    .attr('cx', long)
+                    .attr('cy', lat)
+                    .attr('r', 3)
+                    .style('fill', MAPPING[platform])
+            })
         }
     }
 }
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
+let csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+let liveSocket = new LiveSocket('/live', Socket, {
     longPollFallbackMs: 2500,
     params: {_csrf_token: csrfToken},
     hooks: Hooks
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({barColors: {0: '#29d'}, shadowColor: 'rgba(0, 0, 0, .3)'})
+window.addEventListener('phx:page-loading-start', _info => topbar.show(300))
+window.addEventListener('phx:page-loading-stop', _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
--- lib/sporadic_web/live/firehose_live.ex
+++ lib/sporadic_web/live/firehose_live.ex
@@ -2,33 +2,20 @@ defmodule SporadicWeb.FirehoseLive do
   use SporadicWeb, :live_view
   alias Phoenix.PubSub
 
-  @entries_limit 20
-
   def mount(_params, _session, socket) do
     if connected?(socket), do: PubSub.subscribe(Sporadic.PubSub, "firehose")
-    {:ok, socket |> stream_configure(:buffer, dom_id: &"#{&1[:time]}") |> stream(:buffer, [])}
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <p>Boo</p>
-    <div class="h-screen w-screen" id="map_container" phx-hook="DrawMap"></div>
+    <div class="h-screen w-screen" id="map_container">
+      <svg class="h-full w-full" id="map_svg" phx-hook="DrawMap"></svg>
+    </div>
     """
-
-    # <table>
-    #   <tbody id="firehose" phx-update="stream">
-    #     <tr :for={{dom_id, entry} <- @streams.buffer} id={dom_id}>
-    #       <td><%= entry[:latitude] %></td>
-    #       <td><%= entry[:longitude] %></td>
-    #       <td><%= entry[:time] %></td>
-    #       <td><%= entry[:platform] %></td>
-    #     </tr>
-    #   </tbody>
-    # </table>
-    # """
   end
 
   def handle_info({:feed, entry}, socket) do
-    {:noreply, socket |> stream_insert(:buffer, entry, limit: -@entries_limit)}
+    {:noreply, socket |> push_event(:feed, entry)}
   end
 end
